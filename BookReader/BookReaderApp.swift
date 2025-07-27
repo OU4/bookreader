a4a62ef0dd0151489aeb35c24de1bc5e232deee9
ou4 <c00rksa@gmail.com>
2025-07-28 01:55:56 +0300

    Initial Commit

---
+++ BookReader/BookReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderApp.swift
+//  BookReader
+//
+//  Created by Abdulaziz dot on 28/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookReaderTests/BookReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookReaderTests.swift
+//  BookReaderTests
+//
+//  Created by Abdulaziz dot on 28/07/2025.
+//
+
+import Testing
+@testable import BookReader
+
+struct BookReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
